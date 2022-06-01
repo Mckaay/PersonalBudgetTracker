467b7e4b96cad37eed5e4bcac5312f153f1a5477
Mckay <michal1nycz@gmail.com>
2022-06-01 13:40:02 +0200

    Added getters and setters in User Class

---
+++ User.h
@@ -0,0 +1,32 @@
+#ifndef USER_H
+#define USER_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class User
+{
+    int ID;
+    string login;
+    string password;
+
+public:
+    User(int ID = 1, string login = "admin", string password = "admin");
+    void setId(int ID);
+    void setLogin(string login);
+    void setPassword(string password);
+
+    int getId();
+    string getLogin();
+    string getPassword();
+
+
+
+};
+
+
+
+
+#endif
