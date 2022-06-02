1cbb3eafc9f85fa246eb493147d41cc1c8242d6c
Mckay <michal1nycz@gmail.com>
2022-06-02 11:31:26 +0200

    Added and implemented necessary time functions

---
+++ Time.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <time.h>
+#include <algorithm>
+#include <map>
+#include <string>
+
+using namespace std;
+
+class Time
+{
+public:
+    static string getCurrentDate();
+    static bool checkIfValidDate(int date);
+    static bool isLeapYear(int year);
+    static int daysInAMonth(int monthNumber,bool leapYear);
+    static int convertStringDateToIntDate(string date);
+    static int getYearFromDate(string date);
+    static int getMonthFromDate(string date);
+    static int getDayFromDate(string date);
+    static int currentMaxDate();
+
+};
